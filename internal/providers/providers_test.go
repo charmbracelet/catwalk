6ce57d7aba8a1070eb07d82d74e950eac96f0306
Andrey Nering <andreynering@users.noreply.github.com>
2026-01-06 16:57:36 -0300

    test: add test to check for non-existent default model ids

---
+++ internal/providers/providers_test.go
@@ -0,0 +1,23 @@
+package providers
+
+import (
+	"slices"
+	"testing"
+)
+
+func TestValidDefaultModels(t *testing.T) {
+	for _, p := range GetAll() {
+		t.Run(p.Name, func(t *testing.T) {
+			var modelIds []string
+			for _, m := range p.Models {
+				modelIds = append(modelIds, m.ID)
+			}
+			if !slices.Contains(modelIds, p.DefaultLargeModelID) {
+				t.Errorf("Default large model %q not found in provider %q", p.DefaultLargeModelID, p.Name)
+			}
+			if !slices.Contains(modelIds, p.DefaultSmallModelID) {
+				t.Errorf("Default small model %q not found in provider %q", p.DefaultSmallModelID, p.Name)
+			}
+		})
+	}
+}
