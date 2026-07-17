83869d4e381e13966570c41f330a45aaf24dd1dc
Moshe Marciano <moshebox@gmail.com>
2026-07-17 17:58:07 +0300

    fix: align OpenAI cache pricing fields

--- pkg/catwalk/provider.go
+++ pkg/catwalk/provider.go
@@ -91,7 +91,6 @@ type Model struct {
 	CostPer1MIn            float64      `json:"cost_per_1m_in"`
 	CostPer1MOut           float64      `json:"cost_per_1m_out"`
 	CostPer1MInCached      float64      `json:"cost_per_1m_in_cached"`
-	CostPer1MCacheWrite    float64      `json:"cost_per_1m_cache_write"`
 	CostPer1MOutCached     float64      `json:"cost_per_1m_out_cached"`
 	ContextWindow          int64        `json:"context_window"`
 	DefaultMaxTokens       int64        `json:"default_max_tokens"`
