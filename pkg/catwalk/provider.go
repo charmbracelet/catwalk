37e97630f820bc83fed21809a97fe201c74ac200
Drew Smirnoff <drew@charm.land>
2026-09-14 13:04:10 +0400

    feat: max_attachments (#589)
    Co-authored-by: iceymoss <ice_moss@163.com>
    Signed-off-by: drew <me@andrinoff.com>

--- pkg/catwalk/provider.go
+++ pkg/catwalk/provider.go
@@ -113,6 +113,7 @@ type Model struct {
 	ReasoningLevels        []string     `json:"reasoning_levels,omitempty"`
 	DefaultReasoningEffort string       `json:"default_reasoning_effort,omitempty"`
 	Capabilities           Capabilities `json:"capabilities"`
+	MaxAttachments         int          `json:"max_attachments,omitempty"`
 	Options                ModelOptions `json:"options,omitzero"`
 }
 
